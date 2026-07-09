49950b5c9f5973fea396e79c687604e3173dc810
Kanav Malik <62351072+kanavm1993@users.noreply.github.com>
2026-07-09 19:58:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-const roles=['Clinical Statistical Programmer','Data Scientist','Actuarial Professional'];
+const roles=['Actuarial Professional','Clinical Statistical Programmer','Data Scientist'];
 let i=0;setInterval(()=>{document.getElementById('typing').textContent=roles[i++%roles.length]},1800);
 const obs=new IntersectionObserver(es=>es.forEach(e=>{if(e.isIntersecting)e.target.classList.add('show')}));
 document.querySelectorAll('.reveal').forEach(x=>obs.observe(x));
